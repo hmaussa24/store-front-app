ad78b82c60c237a2992e2660b32d2237219232c2
Harold Maussa Rivas <hmaussa.stefanini@bicevida.cl>
2025-05-26 15:21:54 -0500

    fix: checkoutpage

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     route('home/:id', 'routes/home.tsx'),
-    route('checkout/:id/:talla/:color', 'routes/CheckoutPage.tsx'),
+    route('checkout/:id/:talla/:color', 'routes/checkoutpage.tsx'),
     route('login', 'routes/login.tsx'),
     route('listar-productos', 'routes/listar-productos.tsx'),
     route('crear-producto', 'routes/crear-productos.tsx'),
