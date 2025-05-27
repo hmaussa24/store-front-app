93fcf64ce1e7071e30492a1c76d8e3430c283b5d
Harold Maussa Rivas <hmaussa.stefanini@bicevida.cl>
2025-05-26 20:24:28 -0500

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,18 +3,14 @@ module.exports = {
     {
       name: 'store-front',
       script: './build/server/index.js',
+      watch: false,
       env: {
         NODE_ENV: 'development',
-        PORT: 5000,
-      },
-      env_test: {
-        NODE_ENV: 'test',
-      },
-      env_staging: {
-        NODE_ENV: 'staging',
+        PORT: 3001,
       },
       env_production: {
         NODE_ENV: 'production',
+        PORT: 3003,
       },
     },
   ],
